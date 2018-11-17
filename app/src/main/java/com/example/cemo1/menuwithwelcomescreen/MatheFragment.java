01b9b8f17bb3e8d54bec959c385878d845fec110
cemo-P <cemo.polat@hs-worms.de>
2018-11-17 23:59:22 +0100

    los button mit Funktion an neu fragment optische und rechtschreibfehler korrigierung und ohne auswahl einer checkbox

--- app/src/main/java/com/example/cemo1/menuwithwelcomescreen/MatheFragment.java
+++ app/src/main/java/com/example/cemo1/menuwithwelcomescreen/MatheFragment.java
@@ -10,17 +10,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class MatheFragment extends Fragment implements View.OnClickListener {
 
     View view;
     public Button LOS;
+    public CheckBox addit,multip,subtra,divis;
+    private Boolean isChecked;
 
     //@Nullable
     @Override
     public View onCreateView( LayoutInflater inflater,  ViewGroup container, Bundle savedInstanceState) {
                view = inflater.inflate(R.layout. fragment_mathe, container, false);
                LOS = view.findViewById(R.id. btnMatlos);
+               addit = view.findViewById(R.id.cbAddition);
+               multip = view.findViewById(R.id.cbMultiplikation);
+               subtra = view.findViewById(R.id.cbSubtraktion);
+               divis = view.findViewById(R.id.cbDivision);
                LOS.setOnClickListener(this);
 
                return view;
@@ -29,12 +37,17 @@ public class MatheFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case R.id. btnMatlos:
-                FragmentTransaction fr = getFragmentManager().beginTransaction();
-                fr.replace(R.id.fragment_container, new MatheAufgabeFragment());
-                fr.commit();
-                break;
+        if(multip.isChecked() || divis.isChecked()||addit.isChecked()||subtra.isChecked()) {
+            switch (v.getId()) {
+                case R.id.btnMatlos:
+                    FragmentTransaction fr = getFragmentManager().beginTransaction();
+                    fr.replace(R.id.fragment_container, new MatheAufgabeFragment());
+                    fr.commit();
+                    break;
+            }
+        }
+        else {
+            Toast.makeText(getActivity(),"Wählen Sie eine Rechenart", Toast.LENGTH_LONG).show();
         }
 
     }
