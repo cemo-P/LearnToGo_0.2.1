3112c78e3b9f043d882d33d48f80657303a76356
cemo-P <cemo.polat@hs-worms.de>
2018-11-19 04:18:20 +0100

    obere werte degistirmiyor kullanicidan gelen werte okuyup überschreiben yapmasi lazim olmuyor

--- app/src/main/java/com/example/cemo1/menuwithwelcomescreen/MatheAufgabeFragment.java
+++ app/src/main/java/com/example/cemo1/menuwithwelcomescreen/MatheAufgabeFragment.java
@@ -8,16 +8,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.Random;
+import java.util.Scanner;
 import java.util.Timer;
 
-public class MatheAufgabeFragment extends Fragment implements View.OnClickListener {
+public class MatheAufgabeFragment extends MatheFragment implements View.OnClickListener {
     View view;
-    private TextView tvZahl1,tvZahl2;
-    private int x,y,z,a,b;
-    public Timer timer;
+    private TextView tvZahl1,tvZahl2, zeit,punkt;
+    private EditText ergbebnis;
+    private int x,y,z;
     public Button start;
     @Nullable
     @Override
@@ -25,6 +27,12 @@ public class MatheAufgabeFragment extends Fragment implements View.OnClickListen
         view = inflater.inflate(R.layout. fragment_mathe_aufgabe, container, false);
         tvZahl1 = view.findViewById(R.id. tvz1);
         tvZahl2 = view.findViewById(R.id. tvz2);
+        zeit = view.findViewById(R.id.tvTime);
+        punkt = view.findViewById(R.id.tvPunktStand);
+        ergbebnis = view.findViewById(R.id.etErgebnis);
+        String ss = String.valueOf(a);
+        punkt.setText(ss);
+
 
 
         start = view.findViewById(R.id. btnStrt);
@@ -37,12 +45,18 @@ public class MatheAufgabeFragment extends Fragment implements View.OnClickListen
     @Override
     public void onClick(View v) {
         Random zu = new Random();
-        x = zu.nextInt(10)+1;
-        y = zu.nextInt(20)+1;
-        String s = String.valueOf(x);
-        String s1 = String.valueOf(y);
-        tvZahl1.setText(s);
-        tvZahl2.setText(s1);
+
+        int i = 0;
+        do {
+
+            x = zu.nextInt(a - b) + 1;
+            y = zu.nextInt(a - b) + 1;
+            String s = String.valueOf(x);
+            String s1 = String.valueOf(y);
+            tvZahl1.setText(s);
+            tvZahl2.setText(s1);
+            i++;
+        }while(i<c);
 
 
     }
--- app/src/main/java/com/example/cemo1/menuwithwelcomescreen/MatheFragment.java
+++ app/src/main/java/com/example/cemo1/menuwithwelcomescreen/MatheFragment.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.LoaderManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,19 +14,24 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MatheFragment extends Fragment implements View.OnClickListener {
 
     View view;
-    public Button LOS;
+    public Button LOS,ubernehmen;
     public CheckBox addit,multip,subtra,divis;
     public EditText etOG, etUG, etTime,etAnzahlAufgabe;
-    public int a=10,b=1;
+    public int a=10,b=1, c=10, d = 10,f,g,h;
+    public String u1,u2,u3,u4;
+    public String[] operand;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container,@NonNull Bundle savedInstanceState) {
                view = inflater.inflate(R.layout. fragment_mathe, container, false);
                LOS = view.findViewById(R.id. btnMatlos);
+               ubernehmen = view.findViewById(R.id.btnUbernehmen);
                addit = view.findViewById(R.id.cbAddition);
                multip = view.findViewById(R.id.cbMultiplikation);
                subtra = view.findViewById(R.id.cbSubtraktion);
@@ -34,13 +40,25 @@ public class MatheFragment extends Fragment implements View.OnClickListener {
                etUG = view.findViewById(R.id.etAnfang);
                etTime = view.findViewById(R.id. etSkeingabe);
                etAnzahlAufgabe = view.findViewById(R.id.etAufgAnzahl);
-            String u1 = String.valueOf(b);
-            String u2 = String.valueOf(a);
-            etUG.setText(u1);
-            etOG.setText(u2);
+
+                u2 = String.valueOf(a);
+                etOG.setText(u2);
+
+                u1 = String.valueOf(b);
+                etUG.setText(u1);
+
+                u3 = String.valueOf(c);
+                etAnzahlAufgabe.setText(u3);
+
+                u4 = String.valueOf(d);
+                etTime.setText(u4);
+
+
+
 
 
                LOS.setOnClickListener(this);
+               ubernehmen.setOnClickListener(this);
 
 
                return view;
@@ -52,11 +70,21 @@ public class MatheFragment extends Fragment implements View.OnClickListener {
 
         if(multip.isChecked() || divis.isChecked()||addit.isChecked()||subtra.isChecked()) {
             switch (v.getId()) {
+
                 case R.id.btnMatlos:
-                    FragmentTransaction fr = getFragmentManager().beginTransaction();
-                    fr.replace(R.id.fragment_container, new MatheAufgabeFragment());
-                    fr.commit();
+                    if(a > b) {
+                        FragmentTransaction fr = getFragmentManager().beginTransaction();
+                        fr.replace(R.id.fragment_container, new MatheAufgabeFragment());
+                        fr.commit();
+                    }
+                    else{
+                        Toast.makeText(getActivity(),"Obere Grenz darf nicht kleiner oder gleich als Untere Grenze sein",Toast.LENGTH_SHORT).show();
+                    }
                     break;
+                case R. id. btnUbernehmen:
+                    a = Integer.parseInt(etOG.getText().toString());
+                    break;
+
             }
         }
         else {
