76326ce44a3368c95e2303dac7c77298d0a22fbc
cemo-P <cemo.polat@hs-worms.de>
2018-11-20 21:39:43 +0100

    Mit activitys und mathe aufgabe activity

--- app/src/main/java/com/example/cemo1/menuwithwelcomescreen/MatheAufgActivity.java
+++ app/src/main/java/com/example/cemo1/menuwithwelcomescreen/MatheAufgActivity.java
@@ -10,25 +10,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
-public class MatheActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
+public class MatheAufgActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout schublade;
-    private Button Los;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mathe);
+        setContentView(R.layout.activity_mathe_aufg);
         Toolbar toolbar = findViewById(R.id. toolbar); //üc cizgili Menuyu gosterir
-        schublade = findViewById(R.id.mathe);
+        schublade = findViewById(R.id.matheaufgabe);
         NavigationView navigationView = findViewById(R.id. nav_view );
         navigationView.setNavigationItemSelectedListener(this);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, schublade,toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         schublade.addDrawerListener(toggle);
         toggle.syncState();
-        Los = findViewById(R. id. Losbtn);
-        Los.setOnClickListener(this);
 
     }
 
@@ -61,8 +57,7 @@ public class MatheActivity extends AppCompatActivity implements NavigationView.O
     }
 
     @Override
-    public void onClick(View v) {
-        Intent los = new Intent(this, MatheAufgActivity.class);
-        startActivity(los);
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
